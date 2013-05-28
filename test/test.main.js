60d4ff1727ad7cd7c4fe4a0133648dc9f13db3b7
Alex Veenendaal <alex@opendoorgonorth.com>
2013-05-28 15:36:41 +0100

    added a clearAll function. Reorganising opening/closing

--- test/test.main.js
+++ test/test.main.js
@@ -2,12 +2,33 @@ require('./common');
 
 var sqlite3 = require('sqlite3').verbose();
 
+var sync;
 
 describe('odgn-entity-sqlite', function(){
     before( function(done){
-        this.db = new sqlite3.Database(':memory:');
+        this.sync = sync = require('../')(odgn,{filename:'entity.sqlite'});
+        odgn.entity.EntityRegistry.use( this.sync );
         done();
     });
+
+    var createEntityRegistry = function(options, callback){
+        if( arguments.length == 1 ){
+            callback = options;
+            options = {};
+        }
+        var eRegistry = odgn.entity.EntityRegistry.create();
+
+        var init = function(){
+            eRegistry.initialise( function(err, registry){
+                callback(err,registry);
+            });    
+        }
+
+        if( options.clearAll )
+            return sync.util.clearAll(init);
+
+        return init();
+    };
 
     describe('main', function(){
         
@@ -17,9 +38,7 @@ describe('odgn-entity-sqlite', function(){
 
         it('should convert a schema to sql', function(){
 
-            var SqliteSync = require('../')(odgn);
-
-            SqliteSync.Schema.register({
+            this.sync.Schema.register({
                 "id":"/schema/entity",
                 "title":"entity",
                 "type":"object",
@@ -34,18 +53,32 @@ describe('odgn-entity-sqlite', function(){
                 "primaryKey":"id"
             });
 
-            assert.equal( SqliteSync.Schema.toSql('/schema/entity'),
+            assert.equal( this.sync.Schema.toSql('/schema/entity'),
                 'CREATE TABLE IF NOT EXISTS tbl_entity( id INTEGER PRIMARY KEY, _status INTEGER, _created_at DATETIME DEFAULT CURRENT_TIMESTAMP, _created_by INTEGER, _updated_at DATETIME DEFAULT CURRENT_TIMESTAMP, _updated_by INTEGER);');
         });
+
+        it('should convert another', function(){
+            var schemaId = this.sync.Schema.register({
+                "id":"/component/data",
+                "type":"object",
+                "properties":{
+                    "name":{ "type":"string" },
+                    "count":{ "type":"integer" }
+                }
+            });
+
+            log.debug( this.sync.Schema.toSql(schemaId) );
+
+        });
     });
+
+    
+    
 
     describe('Entity', function(){
 
         it('should create a new entity with an id', function(done){
             var self = this;
-
-            var SqliteSync = require('../')(odgn,{filename:'entity.sqlite'});
-            odgn.entity.EntityRegistry.use( SqliteSync );
 
             var eRegistry = odgn.entity.EntityRegistry.create();
             var entityId;
@@ -55,7 +88,7 @@ describe('odgn-entity-sqlite', function(){
                     eRegistry.initialise(cb);                    
                 },
                 function(cb){
-                    eRegistry.create(cb);
+                    eRegistry.createEntity(cb);
                 },
                 function(entity,cb){
                     log.debug('created entity ' + entity.id );
@@ -68,8 +101,16 @@ describe('odgn-entity-sqlite', function(){
                 done(); 
             });
         });
+    });
 
+    describe('Component', function(){
+        it('should register a component', function(done){
+            var self = this;
 
+            createEntityRegistry({clearAll:true}, function(err, registry){
+                done()
+            });
+        });
     });
 
 });
