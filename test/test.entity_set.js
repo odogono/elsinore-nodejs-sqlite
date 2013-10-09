95536ce2f04592c79261d36c4b6fbc92bca2a829
Alex Veenendaal <alex@opendoorgonorth.com>
2013-10-09 16:07:30 +0100

    entitySets created from queries

--- test/test.entity_set.js
+++ test/test.entity_set.js
@@ -5,21 +5,46 @@ var sqlite3 = require('sqlite3').verbose();
 var Storage = require('../');
 var Schema = require('../lib/schema');
 
-var initRegistryWithSql = function( sqlitePath, sqlFixturePath, callback ){
-    var registry, storage;
-    Storage.loadSql( sqlitePath, fs.readFileSync( Common.pathFixture(sqlFixturePath)).toString(), function(err, storage){
-        odgnEntity.Registry.create({initialize:true, storage:storage, filename:sqlitePath}, function(err, registry){
-            if( err ) throw err;
-            return callback( err, registry, registry.storage );
-        });
-    });
-}
+// var initRegistryWithSql = function( sqlitePath, sqlFixturePath, callback ){
+//     var registry, storage;
+//     Storage.loadSql( sqlitePath, fs.readFileSync( Common.pathFixture(sqlFixturePath)).toString(), function(err, storage){
+//         odgnEntity.Registry.create({initialize:true, storage:storage, filename:sqlitePath}, function(err, registry){
+//             if( err ) throw err;
+//             return callback( err, registry, registry.storage );
+//         });
+//     });
+// }
 
 
 describe('EntitySet', function(){
 
     describe('Init', function(){
-        it('should initialise from existing', function(done){
+
+        it('should create an entity set', function(done){
+            initRegistryWithSql(':memory:', 'content_sets.sql', function(err, registry, storage){
+
+                var esAttrs = {
+                    select:[    '/component/poi', 
+                                '/component/status', 
+                                {schemaId:'/component/content_set/member'},
+                                {schemaId:'/component/content_set', 
+                                    on:'content_set_member.content_set_id=content_set.entity_id', 
+                                    where:{code:'CSB'} }]
+                };
+
+                registry.createEntitySet( esAttrs, {reload:true}, function(err, entitySet){
+                    // entitySet.forEach( function(entity, es){
+                    //     print_ins( es.getComponent('/component/poi', entity).attributes );
+                    // });
+
+                    assert.equal( entitySet.getComponent( "/component/poi", 106 ).get('title'), 'poi 005');
+                    done();
+                });
+
+            });
+        });
+
+        it.skip('should initialise from existing', function(done){
             var self = this;
             var sqlitePath = 'entity_set.sqlite';
 
@@ -40,27 +65,6 @@ describe('EntitySet', function(){
 
             });
         });
-
-        it.skip('should', function(done){
-            initRegistryWithSql( ':memory:', 'content_sets.sql', function(err, registry, storage){
-
-            });
-        });
-            // Storage.loadSql( sqlitePath, fs.readFileSync( Common.pathFixture('basic.sql')).toString(), function(err, storage){
-            //     // log.info( sqlitePath );
-
-            //     self.registry = odgnEntity.Registry.create({initialize:true, storage:Storage, filename:sqlitePath}, function(err, registry){
-            //         self.storage = registry.storage;
-            //         assert( !self.storage.isNew );
-                    
-            //         self.storage.retrieveComponent('/component/name', {where:"last_name='fixture'"}, function(err, component){
-            //             assert.equal( component.get('first_name'), 'charlie' );
-            //             sh.rm( sqlitePath );
-            //             done();
-            //         });
-
-            //     });
-            // });
     });
 
 });
